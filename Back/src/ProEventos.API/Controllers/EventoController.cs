84e891fd370af33cd226ed09d7bb217170f2739b
gabriel brassaroto <biel_ibipa@hotmail.com>
2021-03-28 12:08:53 -0300

    Criando Model Evento

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]///alterar a rota da api
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento =  new Evento[]{ /// criando os dois eventos fora do get para ser retornado
+                new Evento() {
+                EventoId = 1,
+                Tema = "Angular",
+                Local = "Ibiporã",
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                QtdPessoas = 250,
+                Lote = "1° Lote",
+                ImagemURL = "foto.png"
+            },
+                            new Evento() {
+                EventoId = 2,
+                Tema = "Angular test",
+                Local = "Ibiporã",
+                DataEvento = DateTime.Now.AddDays(3).ToString(),
+                QtdPessoas = 350,
+                Lote = "2° Lote",
+                ImagemURL = "foto1.png"
+            }
+          };
+        public EventoController()///construtor 
+        {
+       
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get() //metodos get retorna todos os eventos
+        {
+            return _evento;
+
+        }
+
+        
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> Get(int id) //metodos get que retorna por id e usa sobrecarga de metodo
+        {
+            return _evento.Where(evento => evento.EventoId == id); //retorna o evento que é igual o id que é pasasdo
+
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id) //objeto que vai vir na requisição 
+        {
+            return $"Exemplo de Put com id = {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id) //objeto que vai vir na requisição 
+        {
+            return $"Exemplo de Post de delete com id = {id}";
+        }
+
+    }
+}
